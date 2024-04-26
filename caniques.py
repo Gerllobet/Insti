7b6a6b39c317b182a08ed064a0247b9633621b4b
Gerllobet <137827945+Gerllobet@users.noreply.github.com>
2024-04-26 14:25:56 +0200

    Update caniques.py

--- caniques.py
+++ caniques.py
@@ -16,32 +16,23 @@ def caniques():
 
 def simulation(n):
   i = int(n)
-  counter = 0
+  YA = 0
+  GA = 0
+  YB = 0
+  GB = 0
   while(i > 0):
     if(caniques() == "YA"):
-      counter += 1
+      YA += 1
+    if(caniques() == "YB"):
+      YB += 1
+    if(caniques() == "GA"):
+      GA += 1
+    if(caniques() == "GB"):
+      GB += 1
     i -= 1
-  return(counter)
+  print("YA = " + YA)
+  print("YB = " + YB)
+  print("GA = " + GA)
+  print("GB = " + GB)
 
-print(simulation(10))
-
-
-import random
-def caniques():
-  PotA = ["YA", "YA", "YA", "YA", "YA", "GA", "GA", "GA", "GA", "GA", "GA", "GA", "GA"]
-  PotB = ["YB", "YB", "GA", "GA", "GA", "GA", "GA", "GA"]
-  
-  M1 = random.randint(0, 12) 
-  M2 = random.randint(0, 12)
-  while(M1 == M2):
-    M2 = randint(0, 12)
-
-  PotB += str(PotA[M1])
-  PotB += str(PotA[M2])
-
-  M3 = random.randint(0, 9)
-  return(PotA[M1], PotA[M2], PotB)
-
-
-
-print(caniques())
+simulation(10)
