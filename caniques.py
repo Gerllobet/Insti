d07fd5a5ec7ad4e0fae2538addc9bc55e345fa7a
Gerllobet <137827945+Gerllobet@users.noreply.github.com>
2024-04-26 14:01:43 +0200

    Update caniques.py

--- caniques.py
+++ caniques.py
@@ -24,3 +24,24 @@ def simulation(n):
   return(counter)
 
 print(simulation(10))
+
+
+import random
+def caniques():
+  PotA = ["YA", "YA", "YA", "YA", "YA", "GA", "GA", "GA", "GA", "GA", "GA", "GA", "GA"]
+  PotB = ["YB", "YB", "GA", "GA", "GA", "GA", "GA", "GA"]
+  
+  M1 = random.randint(0, 12) 
+  M2 = random.randint(0, 12)
+  while(M1 == M2):
+    M2 = randint(0, 12)
+
+  PotB += str(PotA[M1])
+  PotB += str(PotA[M2])
+
+  M3 = random.randint(0, 9)
+  return(PotA[M1], PotA[M2], PotB)
+
+
+
+print(caniques())
